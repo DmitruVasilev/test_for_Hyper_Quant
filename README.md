e020b70cfb61eb03333d008879a13e9c144fa81a
Dmitriu Vasilev <mr.disproziu91@gmail.com>
2018-11-13 18:01:54 +0300

    Init

---
+++ README.md
@@ -0,0 +1,3 @@
+start project: "npm i && npm run start" or "yarn && yarn start"
+
+ gh-pages: [test-project](https://dmitruvasilev.github.io/test_for_Hyper_Quant/)
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+import "./sass/general.sass";
+
+class App extends Component {
+  render() {
+    return (
+      <section>
+        <h1>Test</h1>
+      </section>
+    );
+  }
+}
+
+export default App;
