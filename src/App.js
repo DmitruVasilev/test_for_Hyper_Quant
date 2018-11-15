7b6e30ae8d2539790c0bba5d122fbaca820d3a6a
Dmitriu Vasilev <mr.disproziu91@gmail.com>
2018-11-15 16:10:34 +0300

    Add Schedule block

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
 import Header from "./components/Header";
 import Balance from "./components/Balance";
+import Schedule from "./components/Schedule";
 import "./sass/general.sass";
 
 class App extends Component {
@@ -10,6 +11,7 @@ class App extends Component {
         <Header />
         <main>
           <Balance />
+          <Schedule />
         </main>
         <div>Nav</div>
       </div>
---
+++ src/components/Schedule/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Schedule.sass";
+
+const Schedule = () => (
+  <section className="schedule">
+    <span className="schedule__value">+32.6%</span>
+  </section>
+);
+
+export default Schedule;
